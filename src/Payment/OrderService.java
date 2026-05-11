9b0f9b75c00f5fea4d83edd430597cebf3297853
geeth-67 <geeth.me67@email.com>
2026-05-11 09:41:37 +0530

    11/05/2026

--- src/Payment/OrderService.java
+++ src/Payment/OrderService.java
@@ -1,4 +1,17 @@
 package Payment;
 
 public class OrderService {
+
+    private PaymentService paymentService;
+
+    public OrderService(PaymentService paymentService) {   // coding to the interface
+
+        this.paymentService = paymentService;
+    }
+
+    public void processOrder() {
+
+        System.out.println("Order is processing");
+        paymentService.processPayment();
+    }
 }
